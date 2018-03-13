f2caeee1f73cfe6dc0c29592ad8d042c4880bbad
Hebert <nheb461@ecy.wa.lcl>
2018-03-13 15:59:31 -0700

    Devicycle Core Solution

---
+++ Timber/Timber/Models/PhoneContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Timber.Models
+{
+    public class PhoneContext : DbContext
+    {
+        public PhoneContext(DbContextOptions<PhoneContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Phones> Phone { get; set; }
+    }
+}
